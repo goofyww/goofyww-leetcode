fa2b1ca3f6adf1828853e54252f1efb0f2d10a23
goofyww <akindofattitude@163.com>
2021-07-28 15:48:22 +0800

    选择排序

--- src/main/java/cn/goofyww/leetcode/sort/SelectSort.java
+++ src/main/java/cn/goofyww/leetcode/sort/SelectSort.java
@@ -4,6 +4,9 @@ import java.util.Arrays;
 
 /**
  * 选择排序
+ * 时间复杂度：O(N^2)
+ * 空间复杂度：O(1)
+ * 稳定性：稳定
  */
 public class SelectSort {
 
@@ -14,7 +17,20 @@ public class SelectSort {
     }
 
     public static void sort(int[] nums) {
-        // TODO
+        for (int i = 0; i < nums.length; i++) {
+            int min = nums[i]; // min 表示最小变量值，默认以第一个数值为最小值
+            int index = i;// index用来记录最小值的下标
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] < min) {
+                    min = nums[j];
+                    index = j;
+                }
+            }
+            if (index != i) { // 判断index是否被交换过，当前下标值不是最小数的情况
+                nums[index] = nums[i];
+                nums[i] = min;
+            }
+        }
     }
 
 }
