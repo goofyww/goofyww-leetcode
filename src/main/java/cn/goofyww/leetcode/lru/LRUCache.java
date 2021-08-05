268c32235312e8f8ad242578d85dcc960c594ccd
goofyww <akindofattitude@163.com>
2021-08-06 01:53:20 +0800

    TODO 最近最久未使用淘汰算法

--- src/main/java/cn/goofyww/leetcode/lru/LRUCache.java
+++ src/main/java/cn/goofyww/leetcode/lru/LRUCache.java
@@ -1,4 +1,30 @@
 package cn.goofyww.leetcode.lru;
 
+/**
+ * TODO 最近最久未使用淘汰算法
+ * Least Recently Used
+ * 可以将Cache中的数据组织成任意数据结构（包括 数组/链表），其最大容量为N
+ * 时间复杂度不限
+ */
 public class LRUCache {
+
+    /**
+     * 将 k,v 插入缓存
+     *
+     * @param key
+     * @param value
+     */
+    public void insert(int key, int value) {
+
+    }
+
+    /**
+     * 如果 key 命中缓存返回对应的 value ,否则返回 -1
+     *
+     * @param key
+     * @return
+     */
+    public int query(int key) {
+        return -1;
+    }
 }
